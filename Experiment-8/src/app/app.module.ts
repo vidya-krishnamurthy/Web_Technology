e9a1d6bdad301c67511e59f842469230b477cabb
vidya-krishnamurthy <124667670+vidya-krishnamurthy@users.noreply.github.com>
2024-06-27 12:46:57 +0530

    Add files via upload

---
+++ Experiment-8/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { DataTableComponent } from './data-table/data-table.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DataTableComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
